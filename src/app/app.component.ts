ccd7b30a2471c9525b7295a82ec53abb13bc0ebb
rav <ravzski@gmail.com>
2020-03-17 21:52:40 +0800

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.pug',
   styleUrls: ['./app.component.scss']
 })
+
+//
+// form, na pag click ung submit button,
+// sasabihin niya na firstName is required.
+// directive, attribute modified
+// test@test.com
+// 
+
 export class AppComponent {
-  title = 'training';
+
+  title = 'training'
+  number1 = 1
+  number2 = 2
+  form
+  collection = [
+    {
+      firstName: 'test'
+    },
+    {
+      firstName: 'test2'
+    }
+  ]
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.buildForm()
+  }
+
+  buildForm() {
+    this.form = this.fb.group({
+      lastName: ['', [Validators.required, , Validators.minLength(2)]],
+      firstName: ['', [Validators.required, , Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  addTwoNumbers() {
+    return this.number1 + this.number2
+  }
+
+  submitForm() {
+    console.log(this.form)
+    // HTTP POST to server
+  }
+
+  isFormValid() {
+    return true;
+  }
+
 }
